985a18433febd40187e44d373b8fc37ae8f34578
kurota-wow <comcom.co@icloud.com>
2023-07-31 02:43:45 +0900

    Add migration change

--- db/migrate/20230707122730_add_paperclip_to_spots.rb
+++ db/migrate/20230707122730_add_paperclip_to_spots.rb
@@ -2,6 +2,5 @@ class AddPaperclipToSpots < ActiveRecord::Migration[7.0]
   def change
     remove_column :spots, :image
     add_column :spots, :category, :string
-    add_attachment :spots, :image
   end
 end
--- db/migrate/20230713024111_remove_image_from_spots.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromSpots < ActiveRecord::Migration[7.0]
-  def change
-    remove_attachment :spots, :image
-  end
-end
