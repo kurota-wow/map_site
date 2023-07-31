87ed5204fc031bdb8a2797379862385ae54fb275
kurota-wow <comcom.co@icloud.com>
2023-07-31 10:39:05 +0900

    add change render-build.sh

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,6 @@
-
-Spot.create!([
-  {name: "道後温泉本館", address: "愛媛県松山市道後湯之町５−６", content: "3000年もの歴史がある日本最古級の温泉で、趣のある木造の本館は、建物全体が国の重要文化財。", city: "2", latitude: nil, longitude: nil, category: "観光地", url: "https://dogo.jp/"},
-  {name: "今治市", address: "愛媛県今治市", content: "今治市は、愛媛県北東部に位置する市で、サイクリングやタオル、造船で有名。", city: "1", latitude: 34.0658182, longitude: 132.9976758, category: "観光地", url: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjs-eGu3YqAAxUPh1YBHUUGDmoQFnoECA4QAQ&url=https%3A%2F%2Fwww.city.imabari.ehime.jp%2F&usg=AOvVaw3Or5JqX4cFJMs3ZISZ-_xt&opi=89978449"},
-  {name: "旧別子銅山東平地区", address: "愛媛県新居浜市立川町６５４", content: "東洋のマチュピチュと称される産業遺産群が有名。", city: "1", latitude: nil, longitude: nil, category: "観光地", url: "https://besshi.com/sangyoisan/tonaru/"},
-  {name: "石鎚山", address: "愛媛県上浮穴郡久万高原町若山", content: "登ると山頂に神社があり、広大な景色を一望できる。西日本最高峰の山。", city: "1", latitude: 33.704691, longitude: 133.098526, category: "観光地", url: "https://ishizuchisankei.com/spot/spot01/"},
-  {name: "青島", address: "愛媛県大洲市長浜町青島", content: "猫の楽園と呼ばれる、猫好きにはたまらないスポット。", city: "2", latitude: 33.7363564, longitude: 132.4867406, category: "観光地", url: "https://www.city.ozu.ehime.jp/soshiki/nagahamash/0375.html"},
-  {name: "下灘駅", address: "愛媛県伊予市双海町大久保", content: "写真から人気が出た駅。写真映えスポット、映画の舞台として有名。", city: "2", latitude: 33.656058, longitude: 132.61012, category: "観光地", url: "https://www.city.iyo.lg.jp/machizukuri/kanko/guidemap/jrshimonada.html"},
-  {name: "内子町", address: "愛媛県喜多郡内子町内子", content: "愛媛県の南予地方に位置し、江戸や明治の面影を残す町家や屋敷が建ち並ぶ八日市・護国地区が有名。", city: "3", latitude: 33.5492205, longitude: 132.64621, category: "観光地", url: "https://www.we-love-uchiko.jp/stay-cat/kominka/"},
-  {name: "佐田岬半島", address: "愛媛県西宇和郡伊方町", content: "四国の最西端に位置する日本一細長い半島。", city: "3", latitude: 33.4896014, longitude: 132.3560996, category: "観光地", url: "https://www.sadamisaki.com/"},
-  {name: "フォレストアドベンチャー・西条", address: "愛媛県西条市河之内486ｰ1 本谷公園内", content: "全国にある自然の中で遊べる体験型フォレストアドベンチャー。", city: "1", latitude: nil, longitude: nil, category: "レジャー", url: "https://foret-aventure.jp/"},
-  {name: "とべもりジップライン", address: "愛媛県伊予郡砥部町宮内", content: "全長730ｍの四国最大級のスケールのジップライン。こどもの城・とべ動物園を往復できるコースとなっている。", city: "2", latitude: 33.751182, longitude: 132.795067, category: "レジャー", url: "http://www.i-kodomo.jp/zipline/"},
-  {name: "みきゃん型熱気球の係留搭乗体験", address: "愛媛県上浮穴郡久万高原町上野尻", content: "四国唯一の係留熱気球体験、開催期間が限られており、要予約。", city: "2", latitude: 33.646689, longitude: 132.89826, category: "レジャー", url: "https://www.asoview.com/item/activity/pln3000037898/"},
-  {name: "面河渓", address: "愛媛県上浮穴郡久万高原町若山", content: "エメラルドグリーンに輝く水、仁淀ブルーで有名な仁淀川の最上流部。", city: "2", latitude: 33.704691, longitude: 133.098526, category: "レジャー", url: "https://goodriver.jp/omogo"},
-  {name: "四国カルスト", address: "愛媛県上浮穴郡久万高原町西谷", content: "尾根沿いを行くドライブルート。風車、レストラン、遊歩道があり、自然の絶景を楽しめる。", city: "2", latitude: 33.519229, longitude: 132.946971, category: "レジャー", url: "https://kuma-kanko.com/spot/spot398/"},
-  {name: "滑床渓谷", address: "愛媛県宇和島市野川", content: "自然豊かな渓谷。キャニオニング、キャンプなどで遊べる。　", city: "3", latitude: 33.205626, longitude: 132.608566, category: "レジャー", url: "https://sotoasobi.net/activity/canyoning-showerclimbing/9/38/66/1627/8164"},
-  {name: "西海観光船", address: "愛媛県南宇和郡愛南町船越１５９９", content: "船底に窓があって海底を見られる遊覧船。ダイビングや、海底を散歩するシーウォーカーも楽しめる。", city: "3", latitude: nil, longitude: nil, category: "レジャー", url: "https://nishiumi.info/"},
-  {name: "道の駅 霧の森", address: "愛媛県四国中央市新宮町馬立４４９１−１", content: "新宮村の名産品である新宮茶を使った銘菓「霧の森大福」で有名な道の駅。中にクリームが入った大福。", city: "1", latitude: nil, longitude: nil, category: "グルメ", url: "https://www.kirinomori.co.jp/"},
-  {name: "平家谷そうめん流し", address: "愛媛県八幡浜市保内町宮内８−２６４−２", content: "山の中で素麺流しと釣り堀が楽しめるスポット。夏におすすめ。", city: "3", latitude: nil, longitude: nil, category: "グルメ", url: "https://www.city.yawatahama.ehime.jp/doc/2022060100030/"},
-  {name: "宇和島　鯛めし", address: "宇和島鯛めし", content: "宇和島近海で採れた真鯛の刺身をタレ、卵黄と混ぜ合わせ、熱々のご飯にのせて食べる郷土料理。", city: "3", latitude: 33.8432178, longitude: 132.7709609, category: "グルメ", url: "https://www.taimeshi.jp/"},
-  {name: "大山祇神社", address: "愛媛県今治市大三島町宮浦３３２７", content: "古代のクスノキに囲まれた国指定重要文化財の神社。国宝の武具が所蔵されている。", city: "1", latitude: nil, longitude: nil, category: "歴史", url: "https://oomishimagu.jp"},
-  {name: "村上海賊ミュージアム", address: "愛媛県今治市宮窪町宮窪１２８５ 内", content: "瀬戸内海で活躍した能島村上水軍の歴史を紹介する博物館。", city: "1", latitude: nil, longitude: nil, category: "歴史", url: "https://www.city.imabari.ehime.jp/museum/suigun/"},
-  {name: "松山城", address: "愛媛県松山市丸之内１", content: "松山城は、松山市の中心部にある広大な平山城であり、ロープウェーなどで移動できます。", city: "2", latitude: nil, longitude: nil, category: "歴史", url: "https://www.matsuyamajo.jp"},
-  {name: "高昌寺", address: "愛媛県喜多郡内子町城廻１１７", content: "大きな涅槃の石像で有名な内子のお寺。", city: "3", latitude: nil, longitude: nil, category: "歴史", url: ""},
-  {name: "臥龍山荘", address: "愛媛県大洲市大洲４１１−２", content: "景勝地として知られる清流肱川湖畔にある山荘。国の重要文化財に指定される3つの数寄屋建築や日本庭園がある。", city: "3", latitude: nil, longitude: nil, category: "歴史", url: "https://www.garyusanso.jp/"},
-  {name: "宇和文化の里 開明学校", address: "愛媛県西予市宇和町卯之町３丁目１１０", content: "日本の明治時代の学校教育の風景が感じられる建物。明治の授業体験を受けられます。", city: "3", latitude: nil, longitude: nil, category: "歴史", url: "https://www.city.seiyo.ehime.jp/miryoku/uwachonomachinami/kaimei/index.html"},
-  {name: "宇和島市営闘牛場", address: "愛媛県宇和島市和霊町１丁目４９６−２", content: "年に4回開かれる、うわじま闘牛定期大会が有名。", city: "3", latitude: nil, longitude: nil, category: "歴史", url: "https://www.tougyu.com"},
-  {name: "宇和島城", address: "愛媛県宇和島市丸之内", content: "1596 年頃に丘の上に建てられた城。現在は 3 階建ての天守のみが残っており、見学が可能。", city: "3", latitude: nil, longitude: nil, category: "歴史", url: "https://www.city.uwajima.ehime.jp/site/uwajima-jo/"},
-  {name: "今治タオル 本店", address: "愛媛県今治市東門町５丁目１４−３　", content: "タオル産業の発展を担ってきた柔らかく品質の高い、全国的にも有名な今治タオル。", city: "1", latitude: nil, longitude: nil, category: "お土産", url: "https://www.imabaritowel.jp"},
-  {name: "ポンジュース発祥の地", address: "愛媛県松山市中須賀３丁目１１", content: "愛媛県のみかん産業をどのように発展させるかと言う思いから生まれたポンジュース。愛媛のみかんジュースといえばこれ。", city: "2", latitude: nil, longitude: nil, category: "お土産", url: "http://www.ehime-inryo.co.jp"},
-  {name: "砥部焼陶芸館", address: "愛媛県伊予郡砥部町宮内８３", content: "砥部焼は、愛媛県の砥部町を中心に作られている陶磁器。愛媛県指定無形文化財。", city: "2", latitude: nil, longitude: nil, category: "お土産", url: "https://www.togeikan.com/index.php"}
-])
-Event.create!([
-  {content: "市民パレード・大名武者行列、その他イベントが開催される春の大規模なお祭り。", name: "松山お城祭り", address: "松山城", season: "春", image: nil, date: "4月上旬頃"},
-  {content: "砥部焼の窯元の作品10万点が一堂に集まる「砥部焼まつり」。毎年県内外からの多くの来場者で賑わいます。", name: "砥部焼まつり", address: "愛媛県伊予郡砥部町千足400番地", season: "春", image: nil, date: "4月中旬"},
-  {content: "翠波高原の緩やかな傾斜地一面を菜の花が黄色に染め、当日はイベントが催されるため、家族連れなど行楽客で賑わいます。", name: "翠波高原 菜の花まつり", address: "四国中央市金砂町平野山乙306-1翠波高原", season: "春", image: nil, date: "4月中旬ごろ"},
-  {content: "２日間にわたって伝統的な踊りや屋台、花火が楽しめる今治市の夏祭り。", name: "おんまく", address: "今治港", season: "夏", image: nil, date: "8月初旬"},
-  {content: "「牛鬼（うしおに）」とは全長5～6メートルの山車のこと。牛鬼が市内を練り歩く迫力のパレードや伝統の宇和島おどり、夜空と宇和海を彩る花火大会などで盛り上がります。", name: "うわじま牛鬼まつり", address: "宇和島市内", season: "夏", image: nil, date: "7月下旬"},
-  {content: "大洲の夏から秋にかけての風物詩。", name: "大洲のうかい", address: "肱川", season: "夏", image: nil, date: "6月〜9月"},
-  {content: "愛媛を代表する秋祭である。複数の太鼓台が集まって行なわれるかきくらべが有名。", name: "新居浜太鼓まつり", address: "新居浜", season: "秋", image: nil, date: "10月中旬"},
-  {content: "喧嘩神輿と呼ばれる。神輿と神輿がぶつかり合い、神輿を押し合う様は圧巻。", name: "松山道後秋祭り", address: "愛媛県松山市伊予鉄道後温泉駅前", season: "秋", image: nil, date: "10月初旬"},
-  {content: "菊間町加茂神社の祭事として行われる「お供馬の走り込み」は、愛媛県無形民俗文化財の指定を受けた伝統行事。", name: "菊間祭り", address: "菊間町加茂神社", season: "秋", image: nil, date: "10月中旬"},
-  {content: "大洲城をバックに、澄んだ冬空に花火が打ちあがる。", name: "大洲川まつり花火大会　冬の陣", address: "肱川肱北河原", season: "冬", image: nil, date: "1月初旬"},
-  {content: "四国最大規模の日本庭園内には四季折々の花々が咲き乱れ160本の梅の花見が楽しめます。", name: "南楽園梅まつり", address: "南楽園", season: "冬", image: nil, date: "1月〜3月"},
-  {content: "伊豫豆比古命神社で毎年旧暦正月8日を例祭日として前後3日間開催されるのが、愛媛県で冬を代表する催しとして有名。毎年屋台がずらりと並ぶ。", name: "椿まつり", address: "椿神社", season: "冬", image: nil, date: "1月下旬"}
-])
-User.create!([
-  {email: "dummy1@example.com", password: 'kakakakaka'}
-])
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
