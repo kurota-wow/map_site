ecbb8485a29de94813aca601bb27db01a66d38e3
kurota-wow <comcom.co@icloud.com>
2023-08-18 12:46:35 +0900

    Add production mail host setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,9 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto:  true
   }
+
+  config.action_mailer.default_url_options = { host: 'travel-site-926h.onrender.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
